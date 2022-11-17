f02f9adcb849bf1e44aa3b1ca844e27a8fc560a5
Kk Shinkai <kkshinkai@kkshinkai.moe>
2022-11-17 10:25:11 +0800

    Add some comments

--- promise.js
+++ promise.js
@@ -7,26 +7,26 @@ const REJECTED = 'rejected';
 class MyPromise {
   constructor(executor) {
     this.status = PENDING;
-    this.value = undefined; // resolved 'value' or rejected 'reason'.
+    this.valueOrReason = undefined; // resolved 'value' or rejected 'reason'.
     this.onResolvedCallbacks = [];
     this.onRejectedCallbacks = [];
 
-    let resolve = (res) => {
+    let resolve = res => {
       // 2.1.2.1/2.1.3.1. When fulfilled/rejected, a promise must not transition
       // to any other state.
       if (this.status !== PENDING)
         return;
-      this.value = res;
+      this.valueOrReason = res;
       this.status = RESOLVED;
       this.onResolvedCallbacks.forEach(f => f());
     };
 
-    let reject = (err) => {
+    let reject = err => {
       // 2.1.2.1/2.1.3.1. When fulfilled/rejected, a promise must not transition
       // to any other state.
       if (this.status !== PENDING)
         return;
-      this.value = err;
+      this.valueOrReason = err;
       this.status = REJECTED;
       this.onRejectedCallbacks.forEach(f => f());
     };
@@ -46,15 +46,59 @@ class MyPromise {
 
     let newPromise = new MyPromise((resolve, reject) => {
       switch (this.status) {
+      case PENDING:
+        {
+          // The current promise is pending. We don't know if it will be
+          // fulfilled or rejected. So we need to save both `onFulfilled` and
+          // `onRejected` callbacks. They will be invoked when this promise is
+          // settled. (See `resolve` and `reject` functions in the constructor.)
+          this.onResolvedCallbacks.push(() => {
+            // Why we need to wrap `onFulfilled` and `onRejected` in a
+            // `setTimeout`?
+            //
+            // 2.2.4. `onFulfilled` or `onRejected` must not be called until
+            // the execution context stack contains only platform code.
+            //
+            // 3.1. Here "platform code" means engine, environment, and
+            // promise implementation code. In practice, this requirement
+            // ensures that `onFulfilled` and `onRejected` execute
+            // `asynchronously`, after the event loop turn in which then is
+            // called, and with a fresh stack. This can be implemented with
+            // either a "macro-task" mechanism such as `setTimeout` or
+            // `setImmediate`, or with a "micro-task" mechanism such as
+            // `MutationObserver` or `process.nextTick`. Since the promise
+            // implementation is considered platform code, it may itself
+            // contain a task-scheduling queue or "trampoline" in which the
+            // handlers are called.
+            setTimeout(() => {
+              try {
+                let x = onFulfilled(this.valueOrReason);
+                // `x` might be a promise or a normal value. We need to handle
+                // both cases. See `[[Resolve]](promise, x)` in the spec.
+                resolvePromise(newPromise, x, resolve, reject);
+              } catch (err) {
+                reject(err);
+              }
+            }, 0);
+          });
+
+          this.onRejectedCallbacks.push(() => {
+            setTimeout(() => {
+              try {
+                let x = onRejected(this.valueOrReason);
+                resolvePromise(newPromise, x, resolve, reject);
+              } catch (err) {
+                reject(err);
+              }
+            }, 0);
+          });
+        }
+        break;
       case RESOLVED:
         {
-          // Add a new microtask to the queue.
           setTimeout(() => {
-            // This is an asynchronous operation, try-catch in constructor cannot
-            // catch the exception. We need to try it again here.
             try {
-              // `x` may be a promise or a value.
-              let x = onFulfilled(this.value);
+              let x = onFulfilled(this.valueOrReason);
               resolvePromise(newPromise, x, resolve, reject);
             } catch (err) {
               reject(err);
@@ -66,37 +110,12 @@ class MyPromise {
         {
           setTimeout(() => {
             try {
-              let x = onRejected(this.value);
+              let x = onRejected(this.valueOrReason);
               resolvePromise(newPromise, x, resolve, reject);
             } catch (err) {
               reject(err);
             }
           }, 0);
-        }
-        break;
-      case PENDING:
-        {
-          this.onResolvedCallbacks.push(() => {
-            setTimeout(() => {
-              try {
-                let x = onFulfilled(this.value);
-                resolvePromise(newPromise, x, resolve, reject);
-              } catch (err) {
-                reject(err);
-              }
-            }, 0);
-          });
-
-          this.onRejectedCallbacks.push(() => {
-            setTimeout(() => {
-              try {
-                let x = onRejected(this.value);
-                resolvePromise(newPromise, x, resolve, reject);
-              } catch (err) {
-                reject(err);
-              }
-            }, 0);
-          });
         }
         break;
       }
